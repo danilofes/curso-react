3a98cab4e50244b67f46cc07fd574b4e02119ef5
Danilo Ferreira e Silva <danilofes@gmail.com>
2022-12-06 19:49:58 -0300

    projeto base

--- exercicios/base/src/App.tsx
+++ exercicios/base/src/App.tsx
@@ -1,27 +1,3 @@
-import { createHashRouter, RouterProvider } from "react-router-dom";
-import { carregaCardapio } from "./backend";
-import PedidoProvider from "./PedidoProvider";
-import TelaCardapio from "./TelaCardapio";
-import TelaPedido from "./TelaPedido";
-
-const router = createHashRouter([
-  {
-    path: "/",
-    id: "root",
-    loader: carregaCardapio,
-    children: [
-      { index: true, element: <TelaCardapio /> },
-      { path: "pedido", element: <TelaPedido /> },
-    ],
-  },
-]);
-
-function App() {
-  return (
-    <PedidoProvider>
-      <RouterProvider router={router} />
-    </PedidoProvider>
-  );
+export default function App() {
+  return <div>Projeto base</div>;
 }
-
-export default App;
--- exercicios/base/src/util.ts
+++
@@ -1,3 +0,0 @@
-export function formataValor(n: number) {
-  return "R$ " + n.toFixed(2).replace(".", ",");
-}
